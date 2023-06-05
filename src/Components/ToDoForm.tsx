6ff2e9bbe4926b4305d6f88c8eeb2731b72e9957
TypeErrorEngine2022 <jackychen924@gmail.com>
2023-06-05 18:17:24 +0800

    separated components with batch update and minimal css

---
+++ src/Components/ToDoForm.tsx
@@ -0,0 +1,52 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { ToDoItem } from "./ToDoList";
+
+interface ToDoFormProps {
+    updateItemArr: (helper: (prevArr: ToDoItem[]) => ToDoItem[]) => void;
+}
+
+export function ToDoForm (
+    {updateItemArr}: ToDoFormProps){
+    const [text, setText] = useState<string>(''); // display mode
+    
+    // handle text change in create
+    function handleChange(e: ChangeEvent<HTMLInputElement>) {
+        setText(e.target.value);
+    }
+
+    // submit form to add new item
+    function handleSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        if (text != '') {
+            updateItemArr((prevItemArr: ToDoItem[])=> 
+            [...prevItemArr,  
+            {id: crypto.randomUUID(), content: text, isCompleted: false}]);
+        setText('');
+        }
+    }
+
+    // Display mode: delete all items, regardless of status
+    function handleClear() {
+        updateItemArr(() => []);
+        setText('');
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+        <input 
+            className='itemInput'
+            type="text" 
+            placeholder="Add new event"
+            value = {text}
+            onChange = {handleChange}
+        />
+        <button 
+          className='btn'
+          type='submit'>
+          Add
+        </button>
+        <button onClick={handleClear} className='btn dangerous'>
+          Clear</button>
+      </form>
+    )
+}
---
+++ src/Components/ToDoList.tsx
@@ -0,0 +1,72 @@
+import '../ToDoList.css'
+import { useCallback, useState } from 'react'
+import { ToDoItemBox } from './ToDoItemBox';
+import { ToDoForm } from './ToDoForm'
+import { ToDoFilter } from './ToDoFilter';
+
+export interface ToDoItem{
+    id: string,
+    content: string,
+    isCompleted: boolean
+};
+
+export enum Scope {
+    All = 1,
+    Complete,
+    Incomplete,
+}
+
+export function ToDoList() {
+    const [itemArr, setItemArr] = useState<ToDoItem[]>([]);  
+    const [scope, setScope] = useState<Scope>(Scope.All);
+    
+    function updateItemArr(helper: (prevArr: ToDoItem[]) => ToDoItem[]) {
+        setItemArr(helper(itemArr));
+    }
+
+    function updateScope(sc: Scope) {
+        setScope(sc);
+    }
+    
+      // Display mode: return items arr with corresponding status
+    function scopeFilter() {
+        switch(scope) {
+          case Scope.All:
+              return itemArr;
+          case Scope.Complete:
+              return itemArr.filter(item => item.isCompleted === true);
+          case Scope.Incomplete:
+              return itemArr.filter(item => item.isCompleted === false);
+          default:
+              return itemArr;
+        }
+    }
+
+    const scopeFilterMemo = useCallback(scopeFilter, [itemArr, scope])
+
+    return (
+        <>
+          <h1>To Do List</h1> 
+    
+          <ToDoForm 
+            updateItemArr={updateItemArr}/>
+    
+          <br></br>
+    
+          <ToDoFilter 
+            sc={scope}
+            updateScope={updateScope}/>
+
+          <>
+              {scopeFilterMemo().map(todos =>  
+                  <ul key ={todos.id}>
+                    <ToDoItemBox 
+                      todo= {todos} 
+                      updateItemArr={updateItemArr}
+                    />
+                  </ul>)
+              }
+          </>
+        </>
+      )
+}
